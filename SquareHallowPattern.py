3b595ea0c5374e8226ce76d15d695e1b9466e70d
Lohi26 <lohitachandru@gmail.com>
2024-04-01 13:22:35 +0530

    Square Hallow Pattern

--- SquareHallowPattern.py
+++ SquareHallowPattern.py
@@ -1,11 +1,20 @@
 row=int(input())
-column=int(input())
 temp=row
-while temp>0:
-    print("* ",end=" ")
+while (temp>0):
+    print("*",end="  ")
     temp-=1
-for i in range(1,column-2):
-    print("* ",end=" ")
-while row>0:
-    print("* ",end=" ")
+
+
+
+for i in range(1,row-1):
+    print("\n*",end="  ")
+    flag=row-2
+    while flag>0:
+        print(" ",end="  ")
+        flag-=1
+    print("*",end=" ")
+
+print()
+for i in range(1,row+1):
+    print("*",end="  ")
     row-=1
