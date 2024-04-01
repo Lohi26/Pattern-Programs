f48c2968e997addf9b29496185d1cf40e4415cc6
Lohi26 <lohitachandru@gmail.com>
2024-04-01 12:43:57 +0530

    Left Half Pyramid

--- LeftHalfPyramid.py
+++ LeftHalfPyramid.py
@@ -1,1 +1,15 @@
-
+n=int(input())
+k=1
+x=n-1
+for i in range(1,n):
+    flag=x
+    while flag>0:
+        print(" ",end=" ")
+        flag-=1
+    x-=1
+    temp=k
+    while temp>0:
+        print("*",end=" ")
+        temp-=1
+    k+=1
+    print("\n")
