6d0a3601902d6d36734de29d3ad88e0112578dcb
Lohi26 <lohitachandru@gmail.com>
2024-06-07 11:13:32 +0530

    Diamond Pattern

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -1,1 +1,39 @@
+import java.util.*;
+/**
+ * DiamondPattern
+ */
+public class DiamondPattern {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n=sc.nextInt();
+        int temp=n;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=temp-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+            temp--;
+        }
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=1;j<=temp+1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+            temp++;
+        }
+    }
+}
