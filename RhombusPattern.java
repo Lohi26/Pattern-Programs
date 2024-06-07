5e8a8b75325239b036d54919cf172b45cd3e4cb2
Lohi26 <lohitachandru@gmail.com>
2024-06-07 11:04:34 +0530

    Rhombus Pattern

--- RhombusPattern.java
+++ RhombusPattern.java
@@ -1,1 +1,26 @@
+import java.util.*;
+/**
+ * RhombusPattern
+ */
+public class RhombusPattern {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        System.out.print(" ");
+        for(int i=1;i<=n+1;i++)
+        {
+            if(i>1)
+            {
+                for(int k=1;k<=i;k++)
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
