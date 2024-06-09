bb3b2a2f066005df39b70bb35f16ae81b2a245d1
Lohi26 <lohitachandru@gmail.com>
2024-06-09 11:28:57 +0530

    Square Fill Pattern

--- SquareFillPattern.java
+++ SquareFillPattern.java
@@ -1,1 +1,18 @@
+import java.util.*;
+/**
+ * SquareFillPattern
+ */
+public class SquareFillPattern {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
