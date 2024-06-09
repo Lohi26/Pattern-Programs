a17ffbe65f19ba83af2c9a9bf0008f549b4fd393
Lohi26 <lohitachandru@gmail.com>
2024-06-09 12:18:02 +0530

    Reverse Right Half Pyramid

--- ReverseRightHalfPyramid.java
+++ ReverseRightHalfPyramid.java
@@ -1,1 +1,24 @@
+import java.util.*;
+/**
+ * ReverseRightHalfPyramid
+ */
+public class ReverseRightHalfPyramid {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int k=n-i;k>=1;k--)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
