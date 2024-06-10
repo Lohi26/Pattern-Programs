da84deb89349dd933605d1c7d3cbe472bb3afecd
Lohi26 <lohitachandru@gmail.com>
2024-06-10 09:14:01 +0530

    Reverse Number Triangle Pattern

--- ReverseNumberTrianglePattern.java
+++ ReverseNumberTrianglePattern.java
@@ -1,1 +1,27 @@
+import java.util.*;
+/**
+ * ReverseNumberTrianglePattern
+ */
+public class ReverseNumberTrianglePattern {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n=sc.nextInt();
+        int temp=1,flag=0;
+        for(int i=n;i>=1;i--)
+        {
+            for(int k=1;k<=flag;k++)
+            {
+                System.out.print(" ");
+            }
+            flag++;
+            for(int j=temp;j<=n;j++)
+            {
+                System.out.print(j+" ");
+            }
+            temp++;
+            System.out.println();
+        }
+    }
+}
