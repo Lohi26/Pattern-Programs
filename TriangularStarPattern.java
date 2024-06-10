85e97c6e128c23baae6ee5eb9b352d3854b6c92c
Lohi26 <lohitachandru@gmail.com>
2024-06-10 09:05:21 +0530

    Triangle Star Pattern

--- TriangularStarPattern.java
+++ TriangularStarPattern.java
@@ -1,1 +1,26 @@
+import java.util.*;
+/**
+ * TriangularStarPattern
+ */
+public class TriangularStarPattern {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter numeber of rows:");
+        int n=sc.nextInt();
+        int temp=n-1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=temp;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print("* ");
+            }
+            temp--;
+            System.out.println();
+        }
+    }
+}
