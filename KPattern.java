dba00280e8aba71d47c5158c28687f7b614dfe50
Lohi26 <lohitachandru@gmail.com>
2024-06-10 09:00:51 +0530

    K Pattern

--- KPattern.java
+++ KPattern.java
@@ -1,1 +1,40 @@
+import java.util.*;
+/**
+ * KPattern
+ */
+public class KPattern {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n=sc.nextInt();
+        int temp=0;
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int k=1;k<=temp;k++)
+            {
+                System.out.print(" ");
+            }
+            temp++;
+            System.out.println();
+        }
+        int flag=n-1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int k=1;k<=flag;k++)
+            {
+                System.out.print(" ");
+            }
+            flag--;
+            System.out.println();
+        }
+    }
+}
